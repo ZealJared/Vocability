b85034088f7d646b00a0e0aaa24c921f420bb13b
Zeal Mayfield <zeal.j.mayfield@gmail.com>
2021-01-29 17:05:55 -0800

    Tweak URL again

--- ui/src/api.js
+++ ui/src/api.js
@@ -10,7 +10,7 @@ export default class Api {
     this.savedUser = JSON.parse(localStorage.getItem('user'))
   }
   request (method, url, data, noUser) {
-    url = 'https://vocability.api.zealmayfield.com/' + url
+    url = 'https://vocability-api.zealmayfield.com/' + url
     method = method.toUpperCase()
     let options = {
       method: method
